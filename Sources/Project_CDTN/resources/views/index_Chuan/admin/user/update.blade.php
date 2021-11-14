9f70391e0967a32748319913a8b3f7ba604522bf
nhatcuongle <nhatcuongle0208@gmail.com>
2021-11-14 09:57:33 +0700

    new

--- Sources/Project_CDTN/resources/views/index_Chuan/admin/user/update.blade.php
+++ Sources/Project_CDTN/resources/views/index_Chuan/admin/user/update.blade.php
@@ -7,7 +7,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">User
-                            <small>Thêm</small>
+                            <small>Sửa</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
